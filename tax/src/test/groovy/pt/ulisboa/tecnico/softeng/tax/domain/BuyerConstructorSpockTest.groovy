cb980a0614b8e5306652358c2e7f7c3d4f49378b
Bernardo Esteves <bernardo.esteves@tecnico.ulisboa.pt>
2019-03-07 19:39:00 +0000

    BuyerConstructorSpockTest done closes #25

---
+++ tax/src/test/groovy/pt/ulisboa/tecnico/softeng/tax/domain/BuyerConstructorSpockTest.groovy
@@ -0,0 +1,64 @@
+package pt.ulisboa.tecnico.softeng.tax.domain
+
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException
+import spock.lang.Shared
+import spock.lang.Unroll
+
+class BuyerConstructorSpockTest extends SpockRollbackTestAbstractClass {
+    @Shared def ADDRESS = "Somewhere"
+    @Shared def NAME = "José Vendido"
+    @Shared def NIF = "123456789"
+
+    def irs
+
+
+    @Override
+    def populate4Test() {
+        this.irs = IRS.getIRSInstance()
+    }
+
+    def 'success'() {
+        def buyer = new Buyer(this.irs, NIF, NAME, ADDRESS)
+
+        assert buyer.getNif() == NIF
+        assert buyer.getName() == NAME
+        assert buyer.getAddress() == ADDRESS
+
+        assert IRS.getIRSInstance().getTaxPayerByNIF(NIF) == buyer
+    }
+
+
+    def 'unique NIF'() {
+        given: 'a seller'
+        def seller = new Buyer(this.irs, NIF, NAME, ADDRESS)
+
+        when: 'when creating with same arguments'
+        new Buyer(this.irs, NIF, NAME, ADDRESS)
+
+        then: 'throws an exception'
+        thrown(TaxException)
+        assert IRS.getIRSInstance().getTaxPayerByNIF(NIF) == seller
+
+    }
+
+    @Unroll('#nisf, #name, #address')
+    def 'exceptions'() {
+        when: 'creating a buyer with wrong parameters'
+        new Buyer(irs, nif, name, address)
+
+        then: 'throws an exception'
+        thrown(TaxException)
+
+        where:
+        nif        | name | address
+        null       | NAME | ADDRESS
+        ""         | NAME | ADDRESS
+        "12345678" | NAME | ADDRESS
+        NIF        | null | ADDRESS
+        NIF        | ""   | ADDRESS
+        NIF        | NAME | null
+        NIF        | NAME | ""
+    }
+
+
+}
