93b123a006980876f64396daedf0c5526385b291
André Patrício <andrepatricio98@tecnico.ulisboa.pt>
2019-03-08 11:45:08 +0000

    closes #57

---
+++ tax/src/test/groovy/pt/ulisboa/tecnico/softeng/tax/domain/TaxPayerGetTaxesPerYearMethodsSpockTest.groovy
@@ -0,0 +1,54 @@
+package pt.ulisboa.tecnico.softeng.tax.domain
+
+import org.joda.time.LocalDate
+
+class TaxPayerGetTaxesPerYearMethodsSpockTest {
+    def  SELLER_NIF = "123456788"
+    def  BUYER_NIF = "987654311"
+    def  FOOD = "FOOD"
+    def TAX = 10
+    def date = new LocalDate(2018, 02, 13)
+    
+    def seller
+    def buyer
+    def itemType
+    
+    @Override
+    def populate4Test(){
+        def irs = IRS.getIRSInstance()
+        def seller = new Seller(irs, SELLER_NIF, "José Vendido", "Somewhere")
+        def buyer = new Buyer(irs, BUYER_NIF, "Manuel Comprado", "Anywhere")
+        def itemType = new ItemType(irs, FOOD, TAX)
+    }
+    
+    def "success"(){
+        given:
+        new Invoice(100, new LocalDate(2017, 12, 12), itemType, seller, buyer)
+        new Invoice(100, date, itemType, seller, buyer)
+        new Invoice(100, date, itemType, seller, buyer)
+        new Invoice(50, date, itemType, seller, buyer)
+
+        def toPay = seller.getToPayPerYear()
+
+        expect:
+        toPay.keySet().size() == 2
+        toPay.get(2017) == 10.0d
+        toPay.get(2018) == 25.0d
+
+        def taxReturn = buyer.getTaxReturnPerYear()
+
+        taxReturn.keySet().size() == 2
+        taxReturn.get(2017) == 0.5d
+        taxReturn.get(2018) == 1.25d
+    }
+
+    def "success empty"(){
+        def toPay = seller.getToPayPerYear()
+
+        toPay.keySet().size() == 0
+
+        def taxReturn = buyer.getTaxReturnPerYear()
+
+        taxReturn.keySet().size() == 0
+    }
+}
