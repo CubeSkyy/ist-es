9548c29d624d87e861e105f90904dd08d78f05f1
Ricardo <ricardofsilva@live.com>
2019-03-08 07:37:38 +0000

    Missing one in ClientConstructTor.

--- broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/AdventureConstructorMethodTest.groovy
+++ broker/src/test/groovy/pt/ulisboa/tecnico/softeng/broker/domain/AdventureConstructorMethodTest.groovy
@@ -1,8 +1,8 @@
 package pt.ulisboa.tecnico.softeng.broker.domain
 
-
+import org.joda.time.LocalDate
 import pt.ulisboa.tecnico.softeng.broker.exception.BrokerException
-
+import spock.lang.Shared
 import spock.lang.Unroll
 
 import javax.validation.constraints.Null
@@ -10,20 +10,36 @@ import javax.validation.constraints.Null
 
 class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
 
-    def broker
+    @Shared def broker
     def client
 
-    def adventure
+    @Shared def BROKER_IBAN = "BROKER_IBAN"
+    @Shared def NIF_AS_BUYER = "buyerNIF"
+    @Shared def BROKER_NIF_AS_SELLER = "sellerNIF"
+    @Shared def OTHER_NIF = "987654321"
+    @Shared def CLIENT_NIF = "123456789"
+    @Shared def DRIVING_LICENSE = "IMT1234"
+    @Shared def AGE = 20
+    @Shared def AGE_18 = 18
+    @Shared def MARGIN = 0.3
+    @Shared def CLIENT_IBAN = "BK011234567"
+
+
+    @Shared def begin = new LocalDate(2016, 12, 19)
+    @Shared def end = new LocalDate(2016, 12, 21)
+
 
 
     @Override
     def populate4Test() {
         broker = new Broker("BR01", "eXtremeADVENTURE", BROKER_NIF_AS_SELLER, NIF_AS_BUYER, BROKER_IBAN)
         client = new Client(broker, CLIENT_IBAN, CLIENT_NIF, DRIVING_LICENSE, AGE)
-        adventure = new Adventure(broker,begin,end, client, MARGIN)
+
     }
 
     def 'success'() {
+        given:
+        def adventure = new Adventure(broker,begin,end, client, MARGIN)
 
         expect:
         adventure.getBroker() == broker
@@ -31,49 +47,52 @@ class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
         adventure.getEnd() == end
         adventure.getClient() == client
         adventure.getMargin() == MARGIN
-        this.broker.getAdventureSet().contains(adventure)
+        broker.getAdventureSet().contains(adventure)
 
-        adventure.getPaymentConfirmation() == Null
-        adventure.getActivityConfirmation() == Null
-        adventure.getRoomConfirmation() == Null
+        adventure.getPaymentConfirmation() == null
+        adventure.getActivityConfirmation() == null
+        adventure.getRoomConfirmation() == null
     }
 
-    @Unroll('Adventure creation: #broker, #begin, #end, #client, #margin')
+    @Unroll('Adventure creation: #_broker, #_begin, #_end, #_margin')
     def 'exceptions'() {
         when:
-        new Adventure(broker, begin, end, client, margin)
+        new Adventure(_broker, _begin, _end, client, _MARGIN)
 
         then:
         thrown(BrokerException)
 
         where:
-        broker      | begin      | end      | margin
-        null        | this.begin | this.end | MARGIN
-        this.broker | null       | this.end | MARGIN
-        this.broker | this.begin | null     | MARGIN
+        _broker    | _begin     | _end     | _MARGIN
+        null       | begin      | end      | MARGIN
+        broker     | null       | end      | MARGIN
+        broker     | begin      | null     | MARGIN
     }
 
 
     def 'successEqual18'() {
+        given:
+        def c = new Client(this.broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE + "1", 18)
+        def adventure = new Adventure(broker,begin,end, c, MARGIN)
+
         expect:
-        adventure.getBroker() == this.broker
-        adventure.getBegin() == this.begin
-        adventure.getEnd() == this.end
-        adventure.getAge() == 18
+        adventure.getBroker() == broker
+        adventure.getBegin() == begin
+        adventure.getEnd() == end
+        adventure.getAge() == AGE_18
         adventure.getIban() == CLIENT_IBAN
         adventure.getMargin() == MARGIN
-        this.broker.getAdventureSet().contains(adventure)
+        broker.getAdventureSet().contains(adventure)
 
-        adventure.getPaymentConfirmation() == Null
-        adventure.getActivityConfirmation() == Null
-        adventure.getRoomConfirmation() == Null
+        adventure.getPaymentConfirmation() == null
+        adventure.getActivityConfirmation() == null
+        adventure.getRoomConfirmation() == null
     }
 
     def 'negativeAge'() {
-        given:
-        def c = new Client(this.broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE, 17)
         when:
-        new Adventure(this.broker, this.begin, this.end, c, MARGIN)
+        def c = new Client(broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE,17)
+        new Adventure(broker, begin, end, c, MARGIN)
 
         then:
         thrown(BrokerException)
@@ -82,28 +101,27 @@ class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
 
     def 'successEqual100'() {
         given:
-        def c = new Client(this.broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE, 17)
-        new Adventure(this.broker, this.begin, this.end, c, MARGIN)
+        def c = new Client(broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE + "1", 100)
+        def adventure = new Adventure(broker, begin, end, c, MARGIN)
 
         expect:
-        adventure.getBroker() == this.broker
-        adventure.getBegin() == this.begin
-        adventure.getEnd() == this.end
+        adventure.getBroker() == broker
+        adventure.getBegin() == begin
+        adventure.getEnd() == end
         adventure.getAge() == 100
         adventure.getIban() == CLIENT_IBAN
         adventure.getMargin() == MARGIN
-        this.broker.getAdventureSet().contains(adventure)
+        broker.getAdventureSet().contains(adventure)
 
-        adventure.getPaymentConfirmation() == Null
-        adventure.getActivityConfirmation() == Null
-        adventure.getRoomConfirmation() == Null
+        adventure.getPaymentConfirmation() == null
+        adventure.getActivityConfirmation() == null
+        adventure.getRoomConfirmation() == null
 
     }
     def 'over100'() {
-        given:
-        def c = new Client(this.broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE, 101)
         when:
-        new Adventure(broker, begin, end, c, MARGIN)
+        new Adventure(broker, begin, end,
+                new Client(broker, CLIENT_IBAN, OTHER_NIF, DRIVING_LICENSE, 101), MARGIN)
 
         then:
         thrown(BrokerException)
@@ -118,6 +136,8 @@ class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
     }
 
     def 'success1Amount'(){
+        given:
+        def adventure = new Adventure(broker, begin, end, client, 1);
 
         expect:
         adventure.getBroker() == broker
@@ -126,11 +146,11 @@ class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
         adventure.getAge() == 20
         adventure.getIban() == CLIENT_IBAN
         adventure.getMargin() == 1
-        this.broker.getAdventureSet().contains(adventure)
+        broker.getAdventureSet().contains(adventure)
 
-        adventure.getPaymentConfirmation() == Null
-        adventure.getActivityConfirmation() == Null
-        adventure.getRoomConfirmation() == Null
+        adventure.getPaymentConfirmation() == null
+        adventure.getActivityConfirmation() == null
+        adventure.getRoomConfirmation() == null
     }
     def 'zeroAmount'(){
         when:
@@ -141,6 +161,7 @@ class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
     }
 
     def 'successEqualDates'(){
+        def adventure = new Adventure(broker, begin, begin, client, MARGIN)
 
         expect:
         adventure.getBroker() == broker
@@ -151,9 +172,9 @@ class AdventureConstructorMethodTest extends SpockRollbackTestAbstractClass {
         adventure.getMargin() == MARGIN
         this.broker.getAdventureSet().contains(adventure)
 
-        adventure.getPaymentConfirmation() == Null
-        adventure.getActivityConfirmation() == Null
-        adventure.getRoomConfirmation() == Null
+        adventure.getPaymentConfirmation() == null
+        adventure.getActivityConfirmation() == null
+        adventure.getRoomConfirmation() == null
 
     }
     def 'inconsistentDates'() {
