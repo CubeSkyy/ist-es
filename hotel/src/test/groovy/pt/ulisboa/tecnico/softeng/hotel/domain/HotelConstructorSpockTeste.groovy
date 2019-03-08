3b92726a5ef0e2255fc619cc40597fc44d7741d5
BernardoFaria <bernardo.faria@tecnico.ulisboa.pt>
2019-03-08 11:50:20 +0000

    HotelConstructorSpockTest closes #31

---
+++ hotel/src/test/groovy/pt/ulisboa/tecnico/softeng/hotel/domain/HotelConstructorSpockTeste.groovy
@@ -0,0 +1,136 @@
+package pt.ulisboa.tecnico.softeng.hotel.domain
+
+import org.junit.Assert
+import org.junit.Test
+
+import pt.ist.fenixframework.FenixFramework
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException
+
+class HotelConstructorSpockTeste extends RollbackSpockTestAbstractClass {
+    def IBAN = "IBAN"
+    def NIF = "NIF"
+
+    def HOTEL_NAME = "Londres"
+    def HOTEL_CODE = "XPTO123"
+
+    def PRICE_SINGLE = 20.0
+    def PRICE_DOUBLE = 30.0
+
+    @Override
+    def populate4Test() {
+    }
+
+    def 'success'() {
+        given:
+        def hotel = new Hotel(HOTEL_CODE, HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        expect:
+        HOTEL_NAME == hotel.getName()
+        (hotel.getCode().length() == Hotel.CODE_SIZE)
+        hotel.getRoomSet().size() == 0
+        FenixFramework.getDomainRoot().getHotelSet().size() == 1
+        PRICE_SINGLE == hotel.getPrice(Room.Type.SINGLE)
+        PRICE_DOUBLE == hotel.getPrice(Room.Type.DOUBLE)
+    }
+
+    def 'null code'() {
+        when:
+        Hotel(null, HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'blank code'() {
+        when:
+        Hotel("      ", HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'empty code'() {
+        when:
+        Hotel("", HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'null name'() {
+        when:
+        Hotel(HOTEL_CODE, null, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'blank name'() {
+        when:
+        Hotel(HOTEL_CODE, "  ", NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'empty name'() {
+        when:
+        Hotel(HOTEL_CODE, "", NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'code size less'() {
+        when:
+        Hotel("123456", HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'code size more'() {
+        when:
+        Hotel("12345678", HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'code not unique'() {
+        when:
+        Hotel(HOTEL_CODE, HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+        Hotel(HOTEL_CODE, HOTEL_NAME + " City", NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'nif not unique'() {
+        when:
+        Hotel(HOTEL_CODE, HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+        Hotel(HOTEL_CODE + "_new", HOTEL_NAME + "_New", NIF, IBAN, PRICE_SINGLE, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'negative price single'() {
+        when:
+        Hotel(HOTEL_CODE, HOTEL_NAME, NIF, IBAN, -1.0, PRICE_DOUBLE)
+
+        then:
+        thrown(HotelException)
+    }
+
+    def 'negative price double'() {
+        when:
+        Hotel(HOTEL_CODE, HOTEL_NAME, NIF, IBAN, PRICE_SINGLE, -1.0)
+
+        then:
+        thrown(HotelException)
+    }
+
+
+
+}
