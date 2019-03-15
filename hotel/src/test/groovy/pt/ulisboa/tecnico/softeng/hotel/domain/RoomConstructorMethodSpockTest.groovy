837aecdb2fe660cee040a448b897ce1d6bb68fe3
Bernardo Esteves <bernardo.esteves@tecnico.ulisboa.pt>
2019-03-15 15:59:16 +0000

    fixed RoomConstructorMethodSpockTest

--- hotel/src/test/groovy/pt/ulisboa/tecnico/softeng/hotel/domain/RoomConstructorMethodSpockTest.groovy
+++ hotel/src/test/groovy/pt/ulisboa/tecnico/softeng/hotel/domain/RoomConstructorMethodSpockTest.groovy
@@ -7,49 +7,51 @@ import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type
 import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException
 
 class RoomConstructorMethodSpockTest extends RollbackSpockTestAbstractClass {
-	@Shared
-	def hotel
+    @Shared
+    def hotel
 
-	@Override
-	def populate4Test() {
-		hotel = new Hotel("XPTO123", "Lisboa", "NIF", "IBAN", 20.0, 30.0)
-	}
+    @Override
+    def populate4Test() {
+        hotel = new Hotel("XPTO123", "Lisboa", "NIF", "IBAN", 20.0, 30.0)
+    }
 
-	def 'success'() {
-		def room = new Room(hotel, "01", Type.DOUBLE)
+    def 'success'() {
+        given:
+        def room = new Room(hotel, "01", Type.DOUBLE)
+		
+        expect:
+        room.getHotel() == hotel
+        room.getNumber() == "01"
+        room.getType() == Type.DOUBLE
+        hotel.getRoomSet().size() == 1
+    }
 
-		room.getHotel() == hotel
-		room.getNumber() == "01"
-		room.getType() == Type.DOUBLE
-		hotel.getRoomSet().size() == 1
-	}
+    @Unroll('Room creation: #_hotel, #_number, #_type')
+    def 'exceptions'() {
+        when:
+        new Room(_hotel, _number, _type)
 
-	@Unroll('Room creation: #_hotel, #_number, #_type')
-	def 'exceptions'() {
-		when:
-		new Room(_hotel, _number, _type)
+        then:
+        thrown(HotelException)
 
-		then:
-		thrown(HotelException)
+        where:
+        _hotel | _number | _type
+        null   | "01"    | Type.DOUBLE
+        hotel  | null    | Type.DOUBLE
+        hotel  | ""      | Type.DOUBLE
+        hotel  | "     " | Type.DOUBLE
+        hotel  | "JOSE"  | Type.DOUBLE
+        hotel  | "01"    | null
+    }
 
-		where:
-		_hotel	|	_number	| _type
-		null	|	"01"	| Type.DOUBLE
-		hotel	|	null	| Type.DOUBLE
-		hotel	|	""		| Type.DOUBLE
-		hotel	|	"     "	| Type.DOUBLE
-		hotel	|	"JOSE"	| Type.DOUBLE
-		hotel	|	"01"	| null
-	}
+    def 'nonUniqueRoomNumber'() {
+        given:
+        new Room(hotel, "01", Type.SINGLE)
+        when:
+        new Room(hotel, "01", Type.DOUBLE)
+        then:
+        thrown(HotelException)
+        hotel.getRoomSet().size() == 1
 
-	def 'nonUniqueRoomNumber'(){
-		given:
-		new Room(hotel,"01", Type.SINGLE)
-		when:
-		new Room(hotel,"01", Type.DOUBLE)
-		then:
-		thrown(HotelException)
-		hotel.getRoomSet().size() == 1
-
-	}
+    }
 }
