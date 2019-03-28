3840c143b97953bea01fc0608e0e1d398f1123ae
Ricardo <ricardofsilva@live.com>
2019-03-28 22:56:03 +0000

    closes#94

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BookRoomState.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/BookRoomState.java
@@ -9,20 +9,38 @@ import pt.ulisboa.tecnico.softeng.broker.services.remote.exception.RemoteAccessE
 
 public class BookRoomState extends BookRoomState_Base {
 	public static final int MAX_REMOTE_ERRORS = 10;
-
+	public Type type = Type.SINGLE;
 	@Override
 	public State getValue() {
 		return State.BOOK_ROOM;
 	}
 
+	public void setType(Type t){ type = t;}
 	@Override
 	public void process() {
 		try {
-			RestRoomBookingData bookingData = getAdventure().getBroker().getHotelInterface().reserveRoom(new RestRoomBookingData(Type.SINGLE,
-					getAdventure().getBegin(), getAdventure().getEnd(), getAdventure().getBroker().getNifAsBuyer(),
-					getAdventure().getBroker().getIban(), getAdventure().getID()));
-			getAdventure().setRoomConfirmation(bookingData.getReference());
-			getAdventure().incAmountToPay(bookingData.getPrice());
+
+
+			boolean found = false;
+			//Buscar o bulk associado ao broker desta aventura
+			//Hierarquia: Aventura tem broker que tem Bulks que esses tem várias referencias
+			for (BulkRoomBooking bulk: getAdventure().getBroker().getRoomBulkBookingSet()) {
+				//A referencia do booking data
+				RestRoomBookingData bulkbookingData = bulk.getRoomBookingData4Type(Type.SINGLE.toString());
+				if (bulkbookingData != null){
+					getAdventure().setRoomConfirmation(bulkbookingData.getReference());
+					getAdventure().incAmountToPay(bulkbookingData.getPrice());
+					found = true;
+					break;
+				}
+			}
+			if(!found) {
+				RestRoomBookingData bookingData = getAdventure().getBroker().getHotelInterface().reserveRoom(new RestRoomBookingData(Type.SINGLE,
+						getAdventure().getBegin(), getAdventure().getEnd(), getAdventure().getBroker().getNifAsBuyer(),
+						getAdventure().getBroker().getIban(), getAdventure().getID()));
+				getAdventure().setRoomConfirmation(bookingData.getReference());
+				getAdventure().incAmountToPay(bookingData.getPrice());
+			}
 		} catch (HotelException he) {
 			getAdventure().setState(State.UNDO);
 			return;
